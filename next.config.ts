593363ec6cc7b9c8e68f51249cdc5f5522c67866
Ashley Harwood <ashley.harwood@gosource.com.au>
2024-11-25 23:06:08 +1100

    chore: update config

--- next.config.ts
+++ next.config.ts
@@ -6,25 +6,6 @@ const nextConfig: NextConfig = {
   trailingSlash: false,
   
   assetPrefix: '/untp-playground',
-  
-  async rewrites() {
-    return [
-      {
-        source: '/untp-playground/untp-playground/:path*',
-        destination: '/untp-playground/:path*',
-      }
-    ];
-  },
-  
-  async redirects() {
-    return [
-      {
-        source: '/untp-playground/untp-playground/:path*',
-        destination: '/untp-playground/:path*',
-        permanent: true,
-      }
-    ];
-  }
 };
 
 export default nextConfig;
