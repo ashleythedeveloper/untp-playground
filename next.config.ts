bfbec866b441360865bdbf25585c8be845cd2691
Ashley Harwood <ashley.harwood@gosource.com.au>
2024-11-25 22:15:12 +1100

    chore: update config

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,28 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    basePath: '/untp-playground',
-    
-    images: {
-      path: '/untp-playground/_next/image',
-    },
+  basePath: '/untp-playground',
   
-    trailingSlash: true,
-   
-}
+  trailingSlash: true,
+  
+  assetPrefix: '/untp-playground',
+  
+  images: {
+    path: '/untp-playground/_next/image',
+  },
+  
+  async rewrites() {
+    return {
+      beforeFiles: [
+        {
+          source: '/untp-playground/:path*',
+          destination: '/:path*',
+        },
+      ],
+      afterFiles: [],
+      fallback: [],
+    };
+  },
+};
 
 export default nextConfig;
